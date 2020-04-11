1e21782ea0c6878207488a3ee360458b8a1c80c8
Risa Takata <beat1422@icloud.com>
2020-04-10 21:55:27 -0700

    set the link to github and website

--- src/conponents/works/WorksItemLeft.jsx
+++ src/conponents/works/WorksItemLeft.jsx
@@ -1,16 +1,18 @@
 import React from "react"
 
-const WorksItemLeft = ({url,img,title,description,languages}) => {
+const WorksItemLeft = ({url,git,img,title,description,languages}) => {
     return (
         <div className="works_item reverse_work_item">
             <div className="works-descrition">
                 <h1>{title}</h1>
-                <ul>
+                <ul className="work-language">
                     {languages.map((language,index) => <li key={index}>{language}</li>)}
                 </ul>
                 <p>{description}</p>
-                <button>Website</button>
-                <button>GitHub</button>
+                <ul className="work_button">
+                    <li className="work_button"><a href={url}>Website</a></li>
+                    <li className="work_button"><a href={git}>GitHub</a></li>
+                </ul>
             </div>
             <a href={url}><img src={img} alt="works_img" /></a>
         </div>
--- src/conponents/works/WorksItemRight.jsx
+++ src/conponents/works/WorksItemRight.jsx
@@ -1,17 +1,19 @@
 import React from "react"
 
-const WorksItemRight = ({url,img,title,description,languages}) => {
+const WorksItemRight = ({url,git,img,title,description,languages}) => {
     return (
         <div className="works_item">
             <a href={url}><img src={img} alt="works_img" /></a>
             <div className="works-descrition">
                 <h1>{title}</h1>
-                <ul>
+                <ul className="work-language">
                     {languages.map((language) => <li>{language}</li>)}
                 </ul>
                 <p>{description} </p>
-                <button>Website</button>
-                <button className="github_button">GitHub</button>
+                <ul className="work_button">
+                    <li className="work_button"><a href={url}>Website</a></li>
+                    <li className="work_button"><a href={git}>GitHub</a></li>
+                </ul>
             </div>
         </div>
     )
